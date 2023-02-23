031e3caeb4bfbb70f7f9100572338ef353da7da8
MuhammadAarif07 <muhammadaarif8089@gmail.com>
2023-02-23 12:58:30 +0530

    added

--- src/org/test/Test.java
+++ src/org/test/Test.java
@@ -1,5 +1,13 @@
 package org.test;
 
 public class Test {
+	
+	public static void main(String[] args) {
+		System.out.println("test");
+		System.out.println("sample");
+		System.out.println("12314554564654");
+		
+		
+	}
 
 }
